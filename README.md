0f3f335d6c96b9bb5cb671952816d4849714d7c3
Lara <lbg291@googlemail.com>
2014-02-05 01:47:02 +0000

    Transferring files to git

--- README.md
+++
@@ -1,4 +0,0 @@
-ToDo-App
-========
-
-This is part one of the ToDo App Tutorial by Lazarus Lazaridis
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todo::Application.config.secret_key_base = '826c19d8e78a2475624915ff216b296dc56e2767668e86a02d02922b07f31e91a005bfa5e104bd805c6c05a77e91f45d0662f2551e35116482b956e744d67f9b'
